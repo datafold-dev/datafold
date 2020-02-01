dbf326f5b3ad1e1c3061ed6321718125fe784c16
Daniel Lehmberg <daniel.lehmberg@hm.edu>
2020-02-01 09:17:02 +0100

    WIP: GH is now a valid sklearn estimator (with test)

--- datafold/dynfold/outofsample.py
+++ datafold/dynfold/outofsample.py
@@ -78,7 +78,9 @@ class GeometricHarmonicsInterpolator(KernelMethod, RegressorMixin, MultiOutputMi
             self.eigenvectors_, np.reciprocal(self.eigenvalues_)
         ) @ (self.eigenvectors_.T @ self.y_)
 
-    def _validate(self, X: np.ndarray, y: np.ndarray = None) -> np.ndarray:
+    def _validate(
+        self, X: np.ndarray, y: np.ndarray = None, ensure_min_samples=1
+    ) -> np.ndarray:
 
         check_consistent_length(X, y)
 
@@ -94,7 +96,7 @@ class GeometricHarmonicsInterpolator(KernelMethod, RegressorMixin, MultiOutputMi
             "dtype": "numeric",
             "ensure_2d": True,
             "allow_nd": False,
-            "ensure_min_samples": 1,
+            "ensure_min_samples": ensure_min_samples,
             "ensure_min_features": 1,
         }
 
@@ -107,14 +109,6 @@ class GeometricHarmonicsInterpolator(KernelMethod, RegressorMixin, MultiOutputMi
             kwargs["multi_output"] = True
             kwargs["y_numeric"] = True
             X, y = check_X_y(X, y, **kwargs)
-
-        check_scalar(
-            self.num_eigenpairs,
-            "num_eigenpairs",
-            target_type=(np.integer, int),
-            min_val=1,
-            max_val=X.shape[0] - 1,
-        )
 
         return X, y
 
@@ -146,7 +140,7 @@ class GeometricHarmonicsInterpolator(KernelMethod, RegressorMixin, MultiOutputMi
             ],
         )
 
-        X, _ = self._validate(X)
+        X, _ = self._validate(X, ensure_min_samples=1)
         (
             kernel_matrix,
             _sanity_check_basis,
@@ -161,7 +155,7 @@ class GeometricHarmonicsInterpolator(KernelMethod, RegressorMixin, MultiOutputMi
 
     def fit(self, X, y):
 
-        X, y = self._validate(X, y)
+        X, y = self._validate(X, y=y, ensure_min_samples=2)
         self._setup_kernel()
 
         self.X_ = pcfold.PCManifold(
@@ -173,6 +167,14 @@ class GeometricHarmonicsInterpolator(KernelMethod, RegressorMixin, MultiOutputMi
         )
 
         self.y_ = y
+
+        check_scalar(
+            self.num_eigenpairs,
+            "num_eigenpairs",
+            target_type=(np.integer, int),
+            min_val=1,
+            max_val=self.X_.shape[0] - 1,
+        )
 
         (
             self.kernel_matrix_,
@@ -221,7 +223,7 @@ class GeometricHarmonicsInterpolator(KernelMethod, RegressorMixin, MultiOutputMi
         # TODO: generalize to all columns (if required...). Note that this will be a
         #  tensor then.
 
-        X, _ = self._validate(X)
+        X, _ = self._validate(X, ensure_min_samples=1)
 
         assert self.X_ is not None and self.y_ is not None  # prevents mypy warnings
 
@@ -265,7 +267,7 @@ class GeometricHarmonicsInterpolator(KernelMethod, RegressorMixin, MultiOutputMi
 
     def score(self, X, y, sample_weight=None, multioutput="raw_values") -> float:
 
-        X, y = self._validate(X, y)
+        X, y = self._validate(X, y=y, ensure_min_samples=1)
         y_pred = self(X)
 
         score = mean_squared_error(
@@ -371,7 +373,7 @@ class MultiScaleGeometricHarmonicsInterpolator(GeometricHarmonicsInterpolator):
         self._aux = phi_l_ @ diagmat_dot_mat(np.reciprocal(mu_l_), phi_l_.T) @ y
 
     def __call__(self, X):
-        X, _ = self._validate(X)
+        X, _ = self._validate(X=X, y=None, ensure_min_samples=1)
 
         kernel_matrix = self.X.compute_kernel_matrix(
             Y=X, row_sums_alpha_fit=self._row_sums_alpha
@@ -382,7 +384,7 @@ class MultiScaleGeometricHarmonicsInterpolator(GeometricHarmonicsInterpolator):
     def fit(
         self, X: np.ndarray, y=None, **fit_params
     ) -> "MultiScaleGeometricHarmonicsInterpolator":
-        X, y = self._validate(X, y)
+        X, y = self._validate(X, y=y, ensure_min_samples=2)
 
         self._multi_scale_optimize(X, y)
         # self._precompute_aux()
