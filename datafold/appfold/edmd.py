fix numerical issue in time shifts, tests for EDMDCV, EDMDCV.score considers folds from cross validation

datafold/appfold/edmd.py
@@ -96,8 +96,13 @@ class EDMD(Pipeline):
         return super(EDMD, self).fit(X=X, y=y, **fit_params)
 
     def predict(self, X: PRE_IC_TYPES, t=None, **predict_params):
-        # TODO. if X is an np.ndarray, it should be converted to a DataFrame that gives
+        # TODO: if X is an np.ndarray, it should be converted to a DataFrame that gives
         #  a description of the initial condition of the time series.
+
+        if t is None:
+            # Note it cannot simply take the time values from fit, as some time series
+            # may have started at different times (esp. during cross fitting)
+            raise NotImplementedError("")
 
         if isinstance(X, pd.Series):
             X = pd.DataFrame(X).T
@@ -116,32 +121,32 @@ class EDMD(Pipeline):
         return X_ts
 
     def score(self, X: TSCDataFrame, y=None, sample_weight=None):
-        """Docu note: y is kept for consistency , but y is basically X_test"""
-
+        """Docu note: y is kept for consistency to sklearn, but should always be None."""
         assert y is None
-
-        Xt = X
-        for _, name, transform in self._iter(with_final=False):
-            Xt = transform.transform(Xt)
 
         score_params = {}
         if sample_weight is not None:
             score_params["sample_weight"] = sample_weight
 
-        # get initial states in latent space.
-        # important note: during .transform() samples can be discarted (e.g. when
-        # applying Takens)
-        X_latent_ic = Xt.initial_states_df()
+        Xt = X
 
-        X_latent_ts = self.steps[-1][-1].predict(
-            X=X_latent_ic, t=Xt.time_values(unique_values=True)
-        )
+        # exclude last (the DMD model) because the
+        for _, name, dict_step in self._iter(with_final=False):
+            Xt = dict_step.transform(Xt)
 
+        X_latent_ts_folds = []
+        for X_latent_ic, time_values in Xt.tsc.initial_states_folds():
+            current_ts = self.steps[-1][-1].predict(X=X_latent_ic, t=time_values)
+            X_latent_ts_folds.append(current_ts)
+
+        X_latent_ts = pd.concat(X_latent_ts_folds, axis=0)
         X_est_ts = self._inverse_transform_latent_time_series(X_latent_ts)
 
+        # Important note for getting initial states in latent space:
+        # during .transform() samples can be discarted (e.g. when applying Takens)
+        # This means that in the latent space there can be less samples than in the
+        # "pyhsical" space and this is corrected:
         if X.shape[0] > X_est_ts.shape[0]:
-            # Adapt X if time series samples are discareded during transform, and not
-            # recovered during inverse_transform (e.g. for Takens)
             X = X.select_times(time_points=X_est_ts.time_values(unique_values=True))
 
         return self._score_eval(X, X_est_ts, sample_weight)

datafold/appfold/tests/test_edmd.py
@@ -6,22 +6,45 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import pandas.testing as pdtest
+from sklearn.model_selection import GridSearchCV
 
-from datafold.appfold.edmd import EDMDDict, EDMD
+from datafold.appfold.edmd import EDMD, EDMDCV, EDMDDict
 from datafold.pcfold.timeseries import TSCDataFrame
+from datafold.pcfold.timeseries.metric import TSCKfoldSeries, TSCKFoldTime
 from datafold.pcfold.timeseries.transform import (
+    TSCIdentity,
     TSCPrincipalComponent,
     TSCQoiPreprocess,
     TSCTakensEmbedding,
-    TSCIdentity,
 )
 
 
 class EDMDTest(unittest.TestCase):
-    def setUp(self) -> None:
+    def _setup_sine_wave_data(self) -> TSCDataFrame:
         time = np.linspace(0, 2 * np.pi, 100)
         df = pd.DataFrame(np.sin(time) + 10, index=time, columns=["sin"])
-        self.sine_wave_tsc = TSCDataFrame.from_single_timeseries(df)
+        return TSCDataFrame.from_single_timeseries(df)
+
+    def _setup_multi_sine_wave_data(self) -> TSCDataFrame:
+        time = np.linspace(0, 2 * np.pi, 100)
+
+        omega = 1.5
+
+        for i in range(1, 11):
+            data = np.sin(i * omega * time)
+            df = pd.DataFrame(data=data, index=time, columns=["sin"])
+            if i == 1:
+                tsc = TSCDataFrame.from_single_timeseries(df)
+            else:
+                tsc = tsc.insert_ts(df)
+
+        self.assertTrue(tsc.is_equal_time_values())
+
+        return tsc
+
+    def setUp(self) -> None:
+        self.sine_wave_tsc = self._setup_sine_wave_data()
+        self.multi_sine_wave_tsc = self._setup_multi_sine_wave_data()
 
     def test_id_dict(self):
         _edmd_dict = EDMDDict(steps=[("id", TSCIdentity())]).fit(self.sine_wave_tsc)
@@ -34,7 +57,7 @@ class EDMDTest(unittest.TestCase):
             _edmd_dict.inverse_transform(self.sine_wave_tsc), self.sine_wave_tsc
         )
 
-    def test_simple_sine_wave(self, plot=False):
+    def test_edmd_dict_sine_wave(self, plot=False):
         _edmd_dict = EDMDDict(
             steps=[
                 ("scale", TSCQoiPreprocess.scale(name="min-max")),
@@ -62,3 +85,100 @@ class EDMDTest(unittest.TestCase):
             ax = self.sine_wave_tsc.plot()
             inverse_dict.plot(ax=ax)
             plt.show()
+
+    def test_edmd_cv_sine_wave(self):
+        # Tests a specific setting of EDMDCV compared to sklearn.GridSearchCV,
+        # where the results are expected to be the same. EDMDCV generalizes aspects
+        # that fail for GridSearchCV
+
+        edmd = EDMD(
+            dict_steps=[
+                # NOTE: in Takens fill-in handle *cannot* be "remove", because sklearn cv
+                #  will because the number of samples changes during transformation (
+                #  EDMDCV could handle this)
+                ("delays", TSCTakensEmbedding(delays=10, fillin_handle=1)),
+                ("pca", TSCPrincipalComponent(n_components=5)),
+            ]
+        )
+
+        # NOTE: cv only TSCKfoldSeries can be compared and is equal to sklearn. not
+        #  E.g. TSCKfoldTime requires to adapt the internal data (setting the time
+        #  series correctly for the DMD model
+        sklearn_cv = GridSearchCV(
+            estimator=edmd,
+            param_grid={"pca__n_components": [5, 10]},
+            cv=TSCKfoldSeries(2),
+            verbose=False,
+            return_train_score=True,
+            n_jobs=None,
+        )
+
+        edmdcv = EDMDCV(
+            estimator=edmd,
+            param_grid={"pca__n_components": [5, 10]},
+            cv=TSCKfoldSeries(2),
+            verbose=False,
+            return_train_score=True,
+            n_jobs=None,
+        )
+
+        sklearn_cv.fit(self.multi_sine_wave_tsc)
+        edmdcv.fit(self.multi_sine_wave_tsc)
+
+        # timings are very unlikely to be the same, so drop them for the comparison:
+        drop_rows = {
+            "mean_fit_time",
+            "std_fit_time",
+            "mean_score_time",
+            "std_score_time",
+        }
+
+        expected_results = pd.DataFrame(sklearn_cv.cv_results_).T.drop(
+            labels=drop_rows, axis=0
+        )
+        actual_results = pd.DataFrame(edmdcv.cv_results_).T.drop(
+            labels=drop_rows, axis=0
+        )
+
+        pdtest.assert_frame_equal(expected_results, actual_results)
+
+    def test_edmdcv_parallel_no_error(self):
+        edmd = EDMD(
+            dict_steps=[
+                ("delays", TSCTakensEmbedding(delays=10, fillin_handle=1)),
+                ("pca", TSCPrincipalComponent(n_components=5)),
+            ]
+        )
+
+        edmdcv = EDMDCV(
+            estimator=edmd,
+            param_grid={"pca__n_components": [2, 4, 6, 8]},
+            cv=TSCKfoldSeries(4),
+            verbose=False,
+            return_train_score=True,
+            n_jobs=-1,
+        )
+
+        edmdcv.fit(self._setup_multi_sine_wave_data())
+        self.assertIsInstance(edmdcv.cv_results_, dict)
+
+    def test_edmdcv_timecv_no_error(self):
+        edmd = EDMD(
+            dict_steps=[
+                ("delays", TSCTakensEmbedding(delays=10, fillin_handle=1)),
+                ("pca", TSCPrincipalComponent(n_components=5)),
+            ]
+        )
+
+        edmdcv = EDMDCV(
+            estimator=edmd,
+            param_grid={"pca__n_components": [2, 4]},
+            cv=TSCKFoldTime(4),
+            verbose=2,
+            return_train_score=True,
+            n_jobs=1,
+        )
+
+        edmdcv.fit(self._setup_multi_sine_wave_data())
+        print(pd.DataFrame(edmdcv.cv_results_).T)
+        self.assertIsInstance(edmdcv.cv_results_, dict)

datafold/dynfold/dmd.py
@@ -112,7 +112,7 @@ class DMDBase(BaseEstimator, TSCPredictMixIn):
 
         if len(np.unique(X.index.get_level_values("time"))) != 1:
             raise NotImplementedError(
-                "Currently all initial conditions have to have the same initial time."
+                "Currently, all initial conditions have to have the same initial time."
             )
         else:  # assumption required
             initial_time = X.index.get_level_values("time")[0]
@@ -126,14 +126,14 @@ class DMDBase(BaseEstimator, TSCPredictMixIn):
                 # always include the initial condition in the time to evaluate
                 t = np.append(initial_time, t)
 
-        return self._evolve_edmd_system(
-            X_ic=X, time_samples=t, post_map=post_map, qoi_columns=qoi_columns
+        return self._evolve_dmd_system(
+            X_ic=X, time_values=t, post_map=post_map, qoi_columns=qoi_columns
         )
 
-    def _evolve_edmd_system(
+    def _evolve_dmd_system(
         self,
         X_ic: pd.DataFrame,
-        time_samples: np.ndarray,
+        time_values: np.ndarray,
         time_invariant=True,
         post_map: Optional[np.ndarray] = None,
         qoi_columns=None,
@@ -146,7 +146,7 @@ class DMDBase(BaseEstimator, TSCPredictMixIn):
         ic
             Initial condition in same space where EDMD was fit. The initial condition may
             be transformed internally.
-        time_samples
+        time_values
             Array of times where the dynamical system should be evaluated.
         dynmatrix
             If not provided, the dynmatrix corresponds to the eigenvectors of EDMD.
@@ -164,7 +164,10 @@ class DMDBase(BaseEstimator, TSCPredictMixIn):
         """
 
         if not np.isin(X_ic.columns, self._qoi_columns).all():
-            raise ValueError("TODO")
+            raise ValueError(
+                "columns between initial condition and columns during fit "
+                "do not match"
+            )
         else:
             # sort, just in case they are given in a different order
             X_ic = X_ic.loc[:, self._qoi_columns]
@@ -197,7 +200,7 @@ class DMDBase(BaseEstimator, TSCPredictMixIn):
             raise RuntimeError("EDMD is not properly fit.")
 
         if time_invariant:
-            shift = np.min(time_samples)
+            shift = np.min(time_values)
         else:
             # If the edmd time is shifted during data (e.g. the minimum processed data
             # starts with time=5, some positive value) then normalize the time_samples
@@ -205,7 +208,7 @@ class DMDBase(BaseEstimator, TSCPredictMixIn):
             # zero.
             shift = self._normalize_shift
 
-        norm_time_samples = time_samples - shift
+        norm_time_samples = time_values - shift
 
         tsc_df = LinearDynamicalSystem(
             mode="continuous", time_invariant=True
@@ -221,8 +224,27 @@ class DMDBase(BaseEstimator, TSCPredictMixIn):
         )
 
         # correct the time shift again to return the correct time according to the
-        # training data
-        tsc_df = tsc_df.tsc.shift_time(shift_t=shift)
+        # training data (not necessarily "normed time steps" [0, 1, 2, ...]
+        # One way is to shift the time again, i.e.
+        #
+        #    tsc_df.tsc.shift_time(shift_t=shift)
+        #
+        # However, this can sometimes introduce numerical noise (forward/backwards
+        # shifting), therefore the user-requested `time_values` set directly into the
+        # index. This way it matches for all time series.
+        #
+        # Because hard-setting the time indices can introduce problems, the following
+        # assert makes sure that both ways match (up to numerical differences).
+        assert (
+            tsc_df.tsc.shift_time(shift_t=shift).time_values(unique_values=True)
+            - time_values
+            < 1e-15
+        ).all()
+
+        # Hard set of time_values
+        tsc_df.index = tsc_df.index.set_levels(
+            time_values, level=1
+        ).remove_unused_levels()
 
         return tsc_df
 