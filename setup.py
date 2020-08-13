b0828c5e37f1cd70352ee9f86018fe7efead0b5e
Daniel Lehmberg <daniel.lehmberg@hm.edu>
2020-08-13 11:02:52 +0200

    adapt version

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     install_requires=[
         "numpy>=0.18.0,<0.19.0",
         "scikit-learn>=0.22.1,<0.23.0",
-        "scipy>=1.4.0,<1.5",
+        "scipy>=1.4.0,<1.6",
         "pandas>=1.0.0,<1.2.0",
         "numexpr>=2.7.1,<3.0.0",
         "matplotlib>=3.2.0",
