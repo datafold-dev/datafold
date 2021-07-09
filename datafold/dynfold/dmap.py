988a58c97008881e0a28d206d8913c1526491815
Daniel Lehmberg <daniel.lehmberg@hm.edu>
2021-07-09 08:25:26 +0200

    update docs

--- datafold/dynfold/dmap.py
+++ datafold/dynfold/dmap.py
@@ -164,14 +164,10 @@ class DiffusionMaps(BaseEstimator, TSCTransformerMixin):
     """Define a diffusion process on point cloud to find meaningful
     geometric descriptions.
 
-    The model can be used for
+    The method can be used for
 
     * non-linear dimension reduction
-    * approximating eigenfunctions of operators (see ``alpha`` parameter):
-
-        - Laplace-Beltrami
-        - Fokker-Plank
-        - Graph Laplacian
+    * approximating eigenfunctions of various operators (see ``alpha`` parameter).
 
     Parameters
     ----------
@@ -217,9 +213,9 @@ class DiffusionMaps(BaseEstimator, TSCTransformerMixin):
     Attributes
     ----------
 
-    X_fit_: PCManifold
+    X_fit_: Untion[PCManifold, TSCDataFrame]
         The training data during fit. The data is required for out-of-sample mappings;
-        the :py:class:`PCManifold` is equipped with kernel :py:class:`DmapKernelFixed`.
+        the object is equipped with kernel :py:class:`DmapKernelFixed`;
         ``np.asarray(X_fit_)`` casts the object to a standard numpy array.
 
     eigenvalues_ : numpy.ndarray
--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import importlib.util
-import io
 import os
 from pathlib import Path
 
