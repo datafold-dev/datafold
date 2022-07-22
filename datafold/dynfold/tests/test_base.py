f0622d5255a059ff4fa3a7583a4254e670a454bc
Daniel Lehmberg <d.lehmberg@tum.de>
2022-07-22 17:02:43 +0200

    additional tests

---
+++ datafold/dynfold/tests/test_base.py
@@ -0,0 +1,97 @@
+import unittest
+
+import numpy as np
+import numpy.testing as nptest
+
+from datafold.dynfold.base import TSCPredictMixin
+from datafold import TSCDataFrame
+
+
+class TestTSCBase(unittest.TestCase):
+
+    def test_predict_mixin01(self):
+        # test with only time values
+        mixin = TSCPredictMixin()
+
+        expected = np.array([0,1,2])
+        actual = mixin._set_and_validate_time_values_predict(time_values=expected, X=None, U=None)
+        nptest.assert_array_equal(actual, expected)
+
+    def test_predict_mixin02(self):
+        # Test with time information in X
+        mixin = TSCPredictMixin()
+
+        expected = np.array([0, 1, 2])
+        X = TSCDataFrame.from_array(np.arange(2), time_values=expected[0])
+        actual = mixin._set_and_validate_time_values_predict(time_values=expected, X=X, U=None)
+        nptest.assert_array_equal(actual, expected)
+
+        with self.assertRaises(ValueError):
+            # raise error because not all values in time_values are greater or equal to
+            # initial condition reference
+            X = TSCDataFrame.from_array(np.arange(2), time_values=expected[1])
+            mixin._set_and_validate_time_values_predict(time_values=expected, X=X, U=None)
+
+        # because reference value in the initial condition is not contained in time_values,
+        # the time value is prepended
+        expected = expected+2
+        X = TSCDataFrame.from_array(np.arange(2), time_values=0)
+        actual = mixin._set_and_validate_time_values_predict(time_values=expected, X=X, U=None)
+        nptest.assert_array_equal(actual, np.append(0, expected))
+
+        X = TSCDataFrame.from_array(np.arange(6).reshape(3,2), time_values=np.array([0,1,2]))
+        actual = mixin._set_and_validate_time_values_predict(time_values=expected, X=X, U=None)
+        nptest.assert_array_equal(actual, expected)
+
+    def test_predict_mixin03(self):
+        # Test with time information in control input U
+        mixin = TSCPredictMixin()
+
+        expected = np.array([0, 1, 2])
+
+        X = TSCDataFrame.from_array(np.arange(2), time_values=expected[0])
+        U = TSCDataFrame.from_array(np.arange(6).reshape(3,2), time_values=expected)
+
+        actual = mixin._set_and_validate_time_values_predict(time_values=expected, X=X, U=U)
+        nptest.assert_array_equal(actual, expected)
+
+        # no time information in X
+        actual = mixin._set_and_validate_time_values_predict(time_values=expected, X=np.array([0,1]), U=U)
+        nptest.assert_array_equal(actual, expected)
+
+        with self.assertRaises(ValueError):
+            X = TSCDataFrame.from_array(np.arange(2), time_values=expected[1])
+            mixin._set_and_validate_time_values_predict(time_values=expected, X=X, U=U)
+
+    def test_validate_time_values(self):
+        mixin = TSCPredictMixin()
+
+        expected = np.array([0,1,2,3])
+        actual = mixin._validate_time_values_format(time_values=expected)
+
+        nptest.assert_array_equal(actual, expected)
+
+        with self.assertRaises(ValueError):
+            mixin._validate_time_values_format(time_values=np.atleast_2d(expected))
+
+        with self.assertRaises(TypeError):
+            mixin._validate_time_values_format(time_values=expected.astype(str))
+
+        with self.assertRaises(ValueError):
+            mixin._validate_time_values_format(time_values="Invalid")
+
+        with self.assertRaises(ValueError):
+            # no support of negative values
+            mixin._validate_time_values_format(time_values=np.array([-1,0,1]))
+
+        with self.assertRaises(ValueError):
+            mixin._validate_time_values_format(time_values=np.array([np.nan,0,1]))
+
+        with self.assertRaises(ValueError):
+            mixin._validate_time_values_format(time_values=np.array([np.inf,0,1]))
+
+        with self.assertRaises(ValueError):
+            mixin._validate_time_values_format(time_values=np.array([0,0,0]))
+
+        with self.assertRaises(ValueError):
+            mixin._validate_time_values_format(time_values=np.array([2,1,0]))
